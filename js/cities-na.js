7ed1a12981c5ab30f73b9b1330b10e95fac4a877
mnathu <mnathu@hotmail.com>
2026-02-20 09:25:44 -0600

    Create cities-na.js

---
+++ js/cities-na.js
@@ -0,0 +1,10 @@
+const NA_CITIES = [
+ {name:"New York",lat:40.7128,lon:-74.0060},
+ {name:"Chicago",lat:41.8781,lon:-87.6298},
+ {name:"Houston",lat:29.7604,lon:-95.3698},
+ {name:"Los Angeles",lat:34.0522,lon:-118.2437},
+ {name:"Minneapolis",lat:44.9778,lon:-93.2650},
+ {name:"Toronto",lat:43.6532,lon:-79.3832},
+ {name:"Calgary",lat:51.0447,lon:-114.0719},
+ {name:"Miami",lat:25.7617,lon:-80.1918}
+];
