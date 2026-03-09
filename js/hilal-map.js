49bcdba18cc376ef3d6611d0482328c5ef1ba5df
mnathu <mnathu@hotmail.com>
2026-03-09 16:33:34 -0500

    Update hilal-map.js

--- js/hilal-map.js
+++ js/hilal-map.js
@@ -1,1 +1,30 @@
+function globalVisibility(date){
 
+let jd=julianDay(date)
+
+let sun=solarCoordinates(jd)
+let moon=moonCoordinates(jd)
+
+let grid=[]
+
+for(let lat=-60;lat<=60;lat+=3){
+
+for(let lon=-180;lon<=180;lon+=3){
+
+let alt=8+Math.sin(lat*RAD)*6
+let elong=elongation(sun.lambda,moon.lon)
+
+let Q=odehQ(alt,elong)
+
+grid.push({
+lat,
+lon,
+cat:odehCategory(Q)
+})
+
+}
+
+}
+
+return grid
+}
